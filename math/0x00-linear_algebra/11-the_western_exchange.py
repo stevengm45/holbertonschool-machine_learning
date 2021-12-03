1abde676a92bc276c3847df3f3911637ec9aed94
stevengm45 <stevengm45@gmail.com>
2021-12-03 12:29:23 -0500

    transposes matrix

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    this function that transposes matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+        transposes a matrix.
+    """
+    return matrix.transpose()
