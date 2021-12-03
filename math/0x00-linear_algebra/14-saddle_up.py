0c9754377503808ffeb84dc664e1cb96cac6a80b
stevengm45 <stevengm45@gmail.com>
2021-12-03 12:45:34 -0500

    performs matrix multiplication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    This function performs matrix multiplication:
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        performs matrix multiplication on two matrices
+    """
+    return np.dot(mat1, mat2)
