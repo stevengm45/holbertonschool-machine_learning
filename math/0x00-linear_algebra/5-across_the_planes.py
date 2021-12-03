3d3ef4cb7ac39dcfcd4c9885b406f3cad25726a9
stevengm45 <stevengm45@gmail.com>
2021-12-03 11:36:31 -0500

    adds two matrices element-wise

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    This function adds two matrices element-wise
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+        Adds two matrices
+    """
+    import numpy as np
+    mat1 = np.matrix(mat1)
+    mat2 = np.matrix(mat2)
+    if mat1.shape != mat2.shape:
+        return None
+    add_matrix = mat1 + mat2
+    return add_matrix.tolist()
+    
